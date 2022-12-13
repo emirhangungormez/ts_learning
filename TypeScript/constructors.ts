8c1986dfc564edb577dcdb5ab11e78b0d8aba73f
Emir <89928334+emirhangungormez@users.noreply.github.com>
2022-12-13 19:10:52 +0300

    Update constructors.ts

--- TypeScript/constructors.ts
+++ TypeScript/constructors.ts
@@ -9,30 +9,23 @@ interface Vehicle {
 }
 
 class Taxi implements Vehicle {
+
+    color: string;
+
+    constructor(location?: Point, color?: string){
+        this.currentLocation = location;
+        this.color = color;
+    }
+
     currentLocation: Point;
     travelTo(point): void {
         console.log(`Taksi x: ${point.x} y: ${point.y} konumuna gidiyor.`);
     }
 }
 
-class Bus implements Vehicle {
-    currentLocation: Point;
-    travelTo(point): void {
-        console.log(`Otobüs x: ${point.x} y: ${point.y} konumuna gidiyor.`);
-    }
-}
-
-let taxi_1: Taxi = new Taxi; // obje olusturumali
+let taxi_1: Taxi = new Taxi({x: 2, y: 5}, 'red');
 taxi_1.travelTo({x:1, y:2});
-taxi_1.currentLocation = {x: 2, y: 5};
+// taxi_1.color = 'red';
 
-console.log(taxi_1.currentLocation.x);
-console.log(taxi_1.currentLocation.y);
-
-let taxi_2 = new Taxi; // obje olusturumali
-taxi_2.travelTo({x:7, y:9});
-taxi_2.currentLocation = {x: 8, y: 3};
-
-let bus_1 = new Bus; // obje olusturumali
-bus_1.travelTo({x:14, y:25});
-bus_1.currentLocation = {x: 4, y: 3};
+let taxi_2: Taxi = new Taxi ({x: 2, y: 5});
+let taxi_3: Taxi = new Taxi ();
