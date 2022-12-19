d0d83d27460c25e4b165f1b2bbe2f933f117df89
Emir <89928334+emirhangungormez@users.noreply.github.com>
2022-12-19 20:48:50 +0300

    Add files via upload

---
+++ TypeScript/access-modifier.ts
@@ -0,0 +1,20 @@
+interface Point {
+    x: number,
+    y: number
+}
+
+interface Vehicle {
+    travelTo(point: Point): void;
+}
+
+class Taxi implements Vehicle {
+
+    constructor(private location?: Point, private color?: string) {}
+
+    travelTo(point: Point): void {
+        console.log(`Taksi, (x: ${this.location.x}; y: ${this.location.y})'dan (x: ${point.x}; y: ${point.y}) konumuna gidiyor.`);
+    }
+}
+
+let taxi_1: Taxi = new Taxi({x: 2, y: 5}, 'red');
+taxi_1.travelTo({x:1, y:2});
